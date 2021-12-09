f0a8b4e8da878e04d1a0f98f8105c2ec450c6d81
Amanda Grimm <95390235+amandagrimm95@users.noreply.github.com>
2021-12-09 10:07:47 -0500

    Rework button and function relationship

--- openCookie.js
+++ openCookie.js
@@ -1,4 +1,9 @@
+let fortune = document.getElementById("fortune");
+const cookieButton = document.getElementById("cookieButton") 
+
 function writeFortune() {
     let fortune = determineFortune();
-    return `Sometime ${fortune[0]}, ${fortune[1]} will ${fortune[2]}...`;
+    fortune.innerHTML = `Sometime ${fortune[0]}, ${fortune[1]} will ${fortune[2]}...`;
 }
+
+cookieButton.onclick = writeFortune();
